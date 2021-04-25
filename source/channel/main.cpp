f834bf74166a6d702405618c32b00047006d68cf
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-04-24 22:01:45 -0400

    Add pd.dat check
    pd.dat will now only be generated if one doesn't exist. This prevents data from being overwritten, while still being able to generate a file for users who don't have pd.dat.

--- source/channel/main.cpp
+++ source/channel/main.cpp
@@ -16,6 +16,7 @@ extern "C" {
 #include "charmangler.h"
 #include "pd_data.h"
 #include "pd_info.h"
+#include "region.h"
 
 #include <libpatcher/libpatcher.h>
 }
@@ -55,34 +56,39 @@ int main(void) {
     InitFreeType((u8 *)noto_sans_jp_regular_otf, noto_sans_jp_regular_otf_size);
     InitGUIThreads();
 
+    char *filepath = PD_GetDataPath();
+
+    s32 fd = ISFS_Open(filepath, ISFS_OPEN_WRITE);
+    if (fd < 0)
+    {
+        // Decrypt file
+        void *pdLocation = PD_GetFileContents();
+        if (pdLocation == NULL) {
+            // It appears something went awry down the line.
+            // TODO: please do proper error handling!
+            printf("An error occurred reading.\n");
+            ExitApp();
+        }
+
+        // Print on screen because we have no other operation for the moment.
+        struct PDInfoBlock *infoBlock = PD_ParseInfoBlock();
+        if (infoBlock == NULL) {
+            printf("Failed to obtain INFO block.\n");
+            ExitApp();
+        }
+
+        u16_to_char(infoBlock->emailAddress);
+        u16_to_char(infoBlock->firstname);
+        u16_to_char(infoBlock->surname);
+        u16_to_char(infoBlock->zipCode);
+        u16_to_char(infoBlock->city);
+        u16_to_char(infoBlock->address);
+        u16_to_char(infoBlock->apartmentNumber);
+        u16_to_char(infoBlock->phoneNumber);
+    }
+
     MainMenu(1);
 
-    // Decrypt file
-    void *pdLocation = PD_GetFileContents();
-    if (pdLocation == NULL) {
-        // It appears something went awry down the line.
-        // TODO: please do proper error handling!
-        printf("An error occurred reading.\n");
-        ExitApp();
-    }
-
-    // Print on screen because we have no other operation for the moment.
-    struct PDInfoBlock *infoBlock = PD_ParseInfoBlock();
-    if (infoBlock == NULL) {
-        printf("Failed to obtain INFO block.\n");
-        ExitApp();
-    }
-
-    printf("Email: %s\n", u16_to_char(infoBlock->emailAddress));
-    printf("First name: %s\n", u16_to_char(infoBlock->firstname));
-    printf("Surname: %s\n", u16_to_char(infoBlock->surname));
-    printf("Zip code: %s\n", u16_to_char(infoBlock->zipCode));
-    printf("City: %s\n", u16_to_char(infoBlock->city));
-    printf("Address: %s\n", u16_to_char(infoBlock->address));
-    printf("Apartment number: %s\n", u16_to_char(infoBlock->apartmentNumber));
-    printf("Phone number: %s\n", u16_to_char(infoBlock->phoneNumber));
-
-    printf("\n\nEverything is created! Press the HOME button to exit.\n");
 
     return 0;
 }
