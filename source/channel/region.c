abed8ad6807c899611bf35c90cda4080d3bf408d
SketchMaster2001 <75850871+SketchMaster2001@users.noreply.github.com>
2021-05-02 18:34:37 -0400

    Take out region detection
    Our channel patches don't require region specific SPD's

--- source/channel/region.c
+++ source/channel/region.c
@@ -4,51 +4,27 @@
 
 #include "helpers.h"
 
-s32 region = 0;
-
-s32 GetRegionTitle() {
-    switch (CONF_GetRegion()) {
-    case CONF_REGION_JP:
-        // "HCCJ"
-        return 0x4843434a;
-    case CONF_REGION_US:
-        // "HCCE"
-        return 0x48434345;
-    case CONF_REGION_EU:
-        // "HCCP"
-        return 0x48434350;
-    default:
-        // Only three regions are supported by the PD SDK library.
-        return 0;
-    }
-}
-
 char *GetTicketPath() {
     // A string such as /title/00010008/4843434a/content/title.tmd is 43
     // characters. We then factor in a necessary null terminator.
     char *path = malloc(44);
-    sprintf(path, "/title/00010008/%08x/content/title.tmd", region);
+    sprintf(path, "/title/00010008/4843434a/content/title.tmd");
     return path;
 }
 
 char *GetTMDPath() {
     // Similarly, /ticket/00010008/4843434a.tik will be 29 + 1 characters.
     char *path = malloc(30);
-    sprintf(path, "/ticket/00010008/%08x.tik", region);
+    sprintf(path, "/ticket/00010008/4843434a.tik");
     return path;
 }
 
 char *PD_InstallTemplates() { return NULL; }
 
 char *PD_GetDataPath() {
-    region = GetRegionTitle();
-    if (region == 0) {
-        return NULL;
-    }
-
     // /title/00010008/4843434a/data/nocopy/pd.dat\0 is 44 characters.
     char *path = malloc(44);
-    sprintf(path, "/title/00010008/%08x/data/nocopy/pd.dat", region);
+    sprintf(path, "/title/00010008/4843434a/data/nocopy/pd.dat");
 
     // Ensure this path exists.
     s32 ret = CreateParentDirs(path);
