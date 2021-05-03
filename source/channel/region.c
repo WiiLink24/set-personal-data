bead41026bbd6dc26569f3b82626295541d378c6
Spotlight <spotlight@joscomputing.space>
2021-05-03 00:22:16 -0500

    Remove malloc for strings
    We no longer need to change the ID due to channel patches.

--- source/channel/region.c
+++ source/channel/region.c
@@ -5,26 +5,18 @@
 #include "helpers.h"
 
 char *GetTicketPath() {
-    // A string such as /title/00010008/4843434a/content/title.tmd is 43
-    // characters. We then factor in a necessary null terminator.
-    char *path = malloc(44);
-    sprintf(path, "/title/00010008/4843434a/content/title.tmd");
-    return path;
+    return "/title/00010008/4843434a/content/title.tmd";
 }
 
 char *GetTMDPath() {
-    // Similarly, /ticket/00010008/4843434a.tik will be 29 + 1 characters.
-    char *path = malloc(30);
-    sprintf(path, "/ticket/00010008/4843434a.tik");
-    return path;
+    return "/ticket/00010008/4843434a.tik";
 }
 
 char *PD_InstallTemplates() { return NULL; }
 
 char *PD_GetDataPath() {
     // /title/00010008/4843434a/data/nocopy/pd.dat\0 is 44 characters.
-    char *path = malloc(44);
-    sprintf(path, "/title/00010008/4843434a/data/nocopy/pd.dat");
+    char *path = "/title/00010008/4843434a/data/nocopy/pd.dat";
 
     // Ensure this path exists.
     s32 ret = CreateParentDirs(path);
